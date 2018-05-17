2a4417ec5a9510e0b769e24e2d3c978567dde9d9
hleluo <609060722@qq.com>
2018-05-17 10:43:12 +0800

    测试http请求

--- src/main/java/com/monsent/common/http/OkHttpCallback.java
+++ src/main/java/com/monsent/common/http/OkHttpCallback.java
@@ -1,13 +1,11 @@
 package com.monsent.common.http;
 
 /**
- * Created by Administrator on 2017/6/30.
+ * Created by luojia on 2018/5/17.
  */
-
 public interface OkHttpCallback {
 
     void onSuccess(HttpResult result);
 
     void onFailure(String msg);
-
 }
